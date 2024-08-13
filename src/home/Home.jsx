15e0c76ed86a0007b66276b643c0db5cde7cb83b
Smruthul12 <msmruthul2002@gmail.com>
2024-08-13 16:19:45 +0530

    first commit

---
+++ src/home/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Banner from './Banner.jsx';
+import HomeCategory from './HomeCategory.jsx';
+import CategoryShowCase from './CategoryShowCase.jsx';
+import Register from './Register.jsx';
+import LocationSprade from './LocationSprade.jsx';
+import AboutUs from './AboutUs.jsx';
+import AppSection from './AppSection.jsx';
+import Sponsor from './Sponsor.jsx';
+
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+      <HomeCategory/>
+      <CategoryShowCase/>
+      <Register/>
+      <LocationSprade/>
+      <AboutUs/>
+      <AppSection/>
+      <Sponsor/>
+    </div>
+  )
+}
+
+export default Home
